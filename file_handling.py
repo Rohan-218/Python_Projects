1f249f355ff27908b487b631011ccc890ab71197
Rohan Tomar <159640350+Rohan-218@users.noreply.github.com>
2026-06-04 16:34:58 +0530

    Implement file reading and writing examples in file_handling.py and add file.txt

--- file_handling.py
+++ file_handling.py
@@ -1,7 +1,15 @@
-# file = open("Readme.md", mode = "r")
-with open("Readme.md", mode = "r") as file:
-
-    data = file.readline()
-    print(data)
+# file = open("file.txt", mode = "r")
+try:
+    with open("file.txt", mode = "r") as file:
+        data = file.read()
+        print(data)
+except FileNotFoundError as e:
+    print("Error:" + str(e))
 
 # file.close()
+
+with open("file.txt", "w") as file:
+    file.writelines(["This is a new file created using python", 
+                     "\nThis is the second line", 
+                     "\nThis is the third line"
+                    ])
